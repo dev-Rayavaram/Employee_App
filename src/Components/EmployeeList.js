6f443214bd80b17409663d96c1854feb5e4892cc
niecey0123 <s.crumpler11@gmail.com>
2020-05-21 12:53:24 -0400

    deploy

---
+++ src/Components/EmployeeList.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { Button, ButtonGroup, Container, Table } from 'reactstrap';
+import AppNavbar from './AppNavBar';
+import { Link } from 'react-router-dom';
+
+class EmployeeList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {employees: [], isLoading: true};
+    this.remove = this.remove.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({isLoading: true});
+
+    fetch('api/nycgeek/employees')
+      .then(response => response.json())
+      .then(data => this.setState({employees: data, isLoading: false}));
+  }
+
+  async remove(id) {
+    await fetch(`/api/nycgeek/employee/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(() => {
+      let updatedEmployees = [...this.state.employees].filter(i => i.id !== id);
+      this.setState({employees: updatedEmployees});
+    });
+  }
+
+  render() {
+    const {employees, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    const employeeList = employees.map(employee => {
+      return <tr key={employee.id}>
+        <td style={{whiteSpace: 'nowrap'}}>{employee.firstName}</td>
+        <td>{employee.lastName}</td>
+        <td>{employee.email}</td>
+        <td>
+          <ButtonGroup>
+            <Button size="sm" color="primary" tag={Link} to={"/employee/" + employee.id}>Edit</Button>
+            <Button size="sm" color="danger" onClick={() => this.remove(employee.id)}>Delete</Button>
+          </ButtonGroup>
+        </td>
+      </tr>
+    });
+
+    return (
+      <div>
+        <AppNavbar/>
+        <Container fluid>
+          <div className="float-right">
+            <Button color="success" tag={Link} to="/employee/new">Add Group</Button>
+          </div>
+          <h3>My Employees List</h3>
+          <Table className="mt-4">
+            <thead>
+            <tr>
+              <th width="20%">First Name</th>
+              <th width="20%">Last Name</th>
+              <th>Email</th>
+              <th width="10%">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            {employeeList}
+            </tbody>
+          </Table>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default EmployeeList;
